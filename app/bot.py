38c80e9fe31a8dd720189f70e0c544e1dba5393e
anton <anton.lakhtion@gmail.com>
2025-07-27 15:29:26 +0300

    - Подключен телеграм бот: /help /start /github

--- app/bot.py
+++ app/bot.py
@@ -1,1 +1,27 @@
-# новый проект
+import telebot
+from telebot.types import Message
+
+from config import API_KEY
+from text_fot_bot import *
+
+bot = telebot.TeleBot(API_KEY)
+
+
+@bot.message_handler(commands=['start', 'START'])
+def start(message: Message) -> None:
+    user_name = message.from_user.first_name or "друг"
+    bot.send_message(message.chat.id, start_text.format(user_name=user_name))
+
+
+@bot.message_handler(commands=['help', 'HELP'])
+def help(message: Message) -> None:
+    bot.send_message(message.chat.id, help_text)
+
+
+@bot.message_handler(commands=['github', 'GITHUB'])
+def send_my_github(message: Message) -> None:
+    # Отправляет ссылку на GitHub проекта
+    bot.send_message(message.chat.id, github_link_text)
+
+
+bot.polling(non_stop=True)
