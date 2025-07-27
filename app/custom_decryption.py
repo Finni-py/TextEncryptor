df5f29dce788027dd84962eddcbb2761251ea23d
anton <anton.lakhtion@gmail.com>
2025-07-27 14:28:17 +0300

    - Добавлена еще одна ступень шифрования и дешифрования(custom)

---
+++ app/custom_decryption.py
@@ -0,0 +1,57 @@
+def decrypt_func_1(arg: tuple[str, str]) -> tuple[str, str]:
+    # Обратная операция к перевороту строки: снова переворачивает сообщение
+    message, key = arg
+    return message[::-1], key[:-1]
+
+
+def decrypt_func_2(arg: tuple[str, str]) -> tuple[str, str]:
+    # Удаляет последние 3 символа из сообщения (обратная операция добавления 3 букв)
+    message, key = arg
+    return message[:-3], key[:-1]
+
+
+def decrypt_func_3(arg: tuple[str, str]) -> tuple[str, str]:
+    # Удаляет первые 3 символа из сообщения (обратная операция добавления 3 цифр в начало)
+    message, key = arg
+    return message[3:], key[:-1]
+
+
+def decrypt_func_4(arg: tuple[str, str]) -> tuple[str, str]:
+    # Удаляет первые 2 символа из сообщения (обратная операция добавления 2 случайных символов в начало)
+    message, key = arg
+    return message[2:], key[:-1]
+
+
+def decrypt_func_5(arg: tuple[str, str]) -> tuple[str, str]:
+    # Удаляет последние 3 символа из сообщения (обратная операция добавления 3 цифр в конец)
+    message, key = arg
+    return message[:-3], key[:-1]
+
+
+def decrypt_func_6(arg: tuple[str, str]) -> tuple[str, str]:
+    # Удаляет первый символ из сообщения (обратная операция добавления 1 буквы в начало)
+    message, key = arg
+    return message[1:], key[:-1]
+
+
+def get_stock_crypt(arg: tuple[str, str]) -> tuple[str, str]:
+    # Расшифровывает ключ и шифр к для расшифровки fernet
+    message, key = arg
+    while key[-1] != '=':
+        last_char = key[-1]
+        if last_char == 'z':
+            message, key = decrypt_func_1((message, key))
+        elif last_char == '7':
+            message, key = decrypt_func_2((message, key))
+        elif last_char == 'f':
+            message, key = decrypt_func_3((message, key))
+        elif last_char == 'k':
+            message, key = decrypt_func_4((message, key))
+        elif last_char == '5':
+            message, key = decrypt_func_5((message, key))
+        elif last_char == '9':
+            message, key = decrypt_func_6((message, key))
+        else:
+            break
+    print('[+] Расшифровано custom')
+    return message, key
--- app/custom_encryption.py
+++ app/custom_encryption.py
@@ -1,1 +1,59 @@
-# новый проект
+import random
+import string
+
+
+def encrypt_func_1(arg: tuple[str, str]) -> tuple[str, str]:
+    # Функция переворачивает сообщение и добавляет 'z' к ключу
+    message, key = arg
+    return message[::-1], key + 'z'
+
+
+def encrypt_func_2(arg: tuple[str, str]) -> tuple[str, str]:
+    # Функция добавляет к сообщению 3 случайные латинские буквы и добавляет '7' к ключу
+    message, key = arg
+    return message + ''.join(random.choice(string.ascii_letters) for _ in range(3)), key + '7'
+
+
+def encrypt_func_3(arg: tuple[str, str]) -> tuple[str, str]:
+    # Функция добавляет в начало сообщения случайное число от 000 до 999 и добавляет 'f' к ключу
+    message, key = arg
+    return f'{random.randint(0, 999):03d}' + message, key + 'f'
+
+
+def encrypt_func_4(arg: tuple[str, str]) -> tuple[str, str]:
+    # Функция добавляет в начало сообщения 2 случайных символа из заданного набора и добавляет 'k' к ключу
+    message, key = arg
+    return ''.join(random.choice('d=-3402_9d93') for _ in range(2)) + message, key + 'k'
+
+
+def encrypt_func_5(arg: tuple[str, str]) -> tuple[str, str]:
+    # Функция добавляет к сообщению случайное число от 000 до 011 и добавляет '5' к ключу
+    message, key = arg
+    return message + f'{random.randint(0, 11):03d}', key + '5'
+
+
+def encrypt_func_6(arg: tuple[str, str]) -> tuple[str, str]:
+    # Функция добавляет в начало сообщения 1 случайную латинскую букву и добавляет '9' к ключу
+    message, key = arg
+    return ''.join(random.choice(string.ascii_letters) for _ in range(1)) + message, key + '9'
+
+
+encryption_funcs = [
+    encrypt_func_1,
+    encrypt_func_2,
+    encrypt_func_3,
+    encrypt_func_4,
+    encrypt_func_5,
+    encrypt_func_6,
+]
+
+
+def get_random_funcs(arg_stock: tuple[str, str]) -> tuple[str, str]:
+    # Применяет случайное количество (от 10 до 50) случайных функций из списка к аргументу
+    arg = arg_stock
+    random_num = random.randint(10, 50)
+    for _ in range(random_num):
+        random_func = random.choice(encryption_funcs)
+        arg = random_func(arg)
+    print('[+] Зашифровано custom')
+    return arg
